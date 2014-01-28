34a6934702a05f900a7803678bf2b81a0eca5ff8
webstroke <jamie@webstroke.co.uk>
2014-01-28 15:37:43 +0000

    Update retina.js, add 'non-retina' class exclusion
    Added if statement at line 32 to prevent finding retina images if class of 'non-retina' is added to image. Useful if retina.js would create a conflict with other on page javascript, for example the jQuery.cycle plugin.

--- src/retina.js
+++ src/retina.js
@@ -29,7 +29,9 @@
       var images = document.getElementsByTagName("img"), retinaImages = [], i, image;
       for (i = 0; i < images.length; i++) {
         image = images[i];
-        retinaImages.push(new RetinaImage(image));
+        if ( image.getAttribute('class').indexOf('non-retina') == -1 ) {
+          retinaImages.push(new RetinaImage(image));
+        }
       }
       existing_onload();
     }
