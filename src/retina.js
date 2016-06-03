bc35ce544761ee9fee5b2031b144aecb780305c5
Onoshko Dan <danon0404@gmail.com>
2016-06-03 13:45:48 +0700

    `window` instead `root`

--- src/retina.js
+++ src/retina.js
@@ -58,11 +58,11 @@
   Retina.isRetina = function() {
     var mediaQuery = '(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)';
 
-    if (root.devicePixelRatio > 1) {
+    if (window.devicePixelRatio > 1) {
       return true;
     }
 
-    if (root.matchMedia && root.matchMedia(mediaQuery).matches) {
+    if (window.matchMedia && window.matchMedia(mediaQuery).matches) {
       return true;
     }
 
